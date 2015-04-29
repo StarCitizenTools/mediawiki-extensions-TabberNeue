141a3db7141aec786579e86cb8ab01200cc1aaff
Noah Manneschmidt <nmanneschmidt@curse.com>
2015-04-29 15:28:57 -0500

    Rewrite to support multiple tab groups on a single page

--- js/tabber.js
+++ js/tabber.js
@@ -9,41 +9,47 @@
  
  (function($) {
 	$.fn.tabber = function() {
-		// create tabs
-		var tabContent = this.find('.tabbertab'),
-		    nav = $('<ul>').addClass('tabbernav');
-		tabContent.each(function() {
-			var title = $(this).attr('title'),
-			    anchor = $('<a>').text(title).attr('title', title).attr('href', 'javascript:void(0);');
-			$('<li>').append(anchor).appendTo(nav);
+		return this.each(function() {
+			// create tabs
+			var $this = $(this), tabContent = $this.find('.tabbertab'),
+			    nav = $('<ul>').addClass('tabbernav');
+			tabContent.each(function() {
+				var anchor = $('<a>').text(this.title).attr('title', this.title).attr('href', 'javascript:void(0);');
+				$('<li>').append(anchor).appendTo(nav);
+			});
+			$this.prepend(nav);
+
+			/**
+			 * Internal helper function for showing content
+			 * @param  string title to show, matching only 1 tab
+			 * @return true if matching tab could be shown
+			 */
+			function showContent(title) {
+				var content = tabContent.filter('[title="' + title + '"]');
+				if (content.length !== 1) return false;
+				tabContent.hide();
+				content.show();
+				nav.find('.tabberactive').removeClass('tabberactive');
+				nav.find('a[title="' + title + '"]').parent().addClass('tabberactive');
+				return true;
+			}
+			// setup initial state
+			var loc = location.hash.replace('#', '');
+			if ( loc == '' || !showContent(loc) ) {
+				showContent(tabContent.first().attr('title'));
+			}
+
+			// Repond to clicks on the nav tabs
+			nav.on('click', 'a', function(e) {
+				var title = $(this).attr('title');
+				e.preventDefault();
+				location.hash = '#' + title;
+				showContent( title );
+			});
+
+			$this.addClass('tabberlive');
 		});
-		this.prepend(nav);
-
-		// setup initial state
-		var displayedContent, loc = location.hash.replace('#', '');;
-		tabContent.hide();
-		if (loc != '') {
-			displayedContent = tabContent.filter('[title="'+loc+'"]')
-		} else {
-			displayedContent = tabContent.first();
-		}
-		displayedContent.show();
-		nav.find('a[title="'+displayedContent.attr('title')+'"]').parent().addClass('tabberactive');
-
-		this.addClass('tabberlive');
-		return this;
 	};
-
-	// Repond to clicks on the nav tabs
-	$(document).on('click', '.tabbernav li a', function(e) {
-		var title = $(this).attr('title');
-		e.preventDefault();
-		location.hash = '#' + title;
-		$('.tabbertab').hide();
-		$('.tabberactive').removeClass('tabberactive');
-		$('.tabbertab[title="' + title + '"]').show();
-		$('.tabbernav li a[title="' + title + '"]').parent().addClass('tabberactive');
-	});
 })(jQuery);
 
 $(document).ready(function() {
