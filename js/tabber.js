49cd5b8cd6ff672a88f29db3534e05f7658e4d50
Alexia E. Smith <washuu@gmail.com>
2018-01-25 13:33:50 -0600

    Fix URI encoded tabs.

--- js/tabber.js
+++ js/tabber.js
@@ -1,61 +1,61 @@
-( function ( $ ) {
-	$.fn.tabber = function () {
-		return this.each( function () {
+(function($) {
+	$.fn.tabber = function() {
+		return this.each(function() {
 			// create tabs
-			var $this = $( this ),
-				tabContent = $this.children( '.tabbertab' ),
-				nav = $( '<ul>' ).addClass( 'tabbernav' ),
+			var $this = $(this),
+				tabContent = $this.children('.tabbertab'),
+				nav = $('<ul>').addClass('tabbernav'),
 				loc;
 
-			tabContent.each( function () {
-				var anchor = $( '<a>' ).text( this.title ).attr( 'title', this.title ).attr( 'href', '#' );
-				$( '<li>' ).append( anchor ).appendTo( nav );
+			tabContent.each(function() {
+				var anchor = $('<a>').text(this.title).attr('title', this.title).attr('href', '#');
+				$('<li>').append(anchor).appendTo(nav);
 
 				// Append a manual word break point after each tab
-				nav.append( $( '<wbr>' ) );
-			} );
+				nav.append($('<wbr>'));
+			});
 
-			$this.prepend( nav );
+			$this.prepend(nav);
 
 			/**
 			 * Internal helper function for showing content
 			 * @param  {string} title to show, matching only 1 tab
 			 * @return {bool} true if matching tab could be shown
 			 */
-			function showContent( title ) {
-				var content = tabContent.filter( '[title="' + title + '"]' );
-				if ( content.length !== 1 ) { return false; }
+			function showContent(title) {
+				var content = tabContent.filter('[title="' + title + '"]');
+				if (content.length !== 1) { return false; }
 				tabContent.hide();
 				content.show();
-				nav.find( '.tabberactive' ).removeClass( 'tabberactive' );
-				nav.find( 'a[title="' + title + '"]' ).parent().addClass( 'tabberactive' );
+				nav.find('.tabberactive').removeClass('tabberactive');
+				nav.find('a[title="' + title + '"]').parent().addClass('tabberactive');
 				return true;
 			}
 
 			// setup initial state
-			loc = location.hash.replace( '#', '' );
-			if ( loc === '' || !showContent( loc ) ) {
-				showContent( tabContent.first().attr( 'title' ) );
+			var tab = decodeURI(location.hash.replace('#', ''));
+			if (tab === '' || !showContent(tab)) {
+				showContent(tabContent.first().attr('title'));
 			}
 
 			// Respond to clicks on the nav tabs
-			nav.on( 'click', 'a', function ( e ) {
-				var title = $( this ).attr( 'title' );
+			nav.on('click', 'a', function(e) {
+				var title = $(this).attr('title');
 				e.preventDefault();
-				if ( history.pushState ) {
-					history.pushState( null, null, '#' + title );
+				if (history.pushState) {
+					history.pushState(null, null, '#' + title);
 					switchTab(title);
 				} else {
 					location.hash = '#' + title;
 				}
-			} );
+			});
 
 			$(window).on('hashchange', function(event) {
 				switchTab(event);
 			});
 
 			function switchTab(event) {
-				var tab = location.hash.replace('#', '');
+				var tab = decodeURI(location.hash.replace('#', ''));
 				if (!tab.length) {
 					showContent(tabContent.first().attr('title'));
 				}
@@ -64,11 +64,11 @@
 				}
 			}
 
-			$this.addClass( 'tabberlive' );
-		} );
+			$this.addClass('tabberlive');
+		});
 	};
-}( jQuery ) );
+}(jQuery));
 
-$( document ).ready( function () {
-	$( '.tabber' ).tabber();
-} );
+$(document).ready(function() {
+	$('.tabber').tabber();
+});
