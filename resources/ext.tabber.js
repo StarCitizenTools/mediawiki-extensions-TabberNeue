3fbf00ba4481b6cc82f90af985e4e86b237f46f0
alistair3149 <alistair31494322@gmail.com>
2019-08-12 19:46:12 -0400

    Code spacing

--- resources/ext.tabber.js
+++ resources/ext.tabber.js
@@ -6,36 +6,36 @@
       var $this = $(this),
         tabContent = $this.children('.tabbertab'),
         loc;
-			var tabcount = 0;
-			var index = new OO.ui.IndexLayout();
+      var tabcount = 0;
+      var index = new OO.ui.IndexLayout();
 
       tabContent.each(function() {
-				tabcount++;
-				CreateTab( this.title, tabcount );
+        tabcount++;
+        CreateTab(this.title, tabcount);
       });
 
-			$('.tabber').append(index.$element);
+      $('.tabber').append(index.$element);
 
-			function CreateTab( title, count ) {
+      function CreateTab(title, count) {
 
-				var content = $('.tabbertab[title="' + title +'"]');
-				//var content = tabContent.filter('[data-hash="' + title + '"]');
+        var content = $('.tabbertab[title="' + title + '"]');
+        //var content = tabContent.filter('[data-hash="' + title + '"]');
 
-				function TabPanelLayout( name, config ) {
-					TabPanelLayout.super.call( this, name, config );
-					this.$element.attr('title', title).attr('data-hash', mw.util.escapeIdForAttribute(title));
-					this.$element.append( content );
-				}
+        function TabPanelLayout(name, config) {
+          TabPanelLayout.super.call(this, name, config);
+          this.$element.attr('title', title).attr('data-hash', mw.util.escapeIdForAttribute(title));
+          this.$element.append(content);
+        }
 
-				OO.inheritClass( TabPanelLayout, OO.ui.TabPanelLayout );
-				TabPanelLayout.prototype.setupTabItem = function () {
-					this.tabItem.setLabel( title );
-				};
+        OO.inheritClass(TabPanelLayout, OO.ui.TabPanelLayout);
+        TabPanelLayout.prototype.setupTabItem = function() {
+          this.tabItem.setLabel(title);
+        };
 
-				var tabPanel = new TabPanelLayout( tabcount );
+        var tabPanel = new TabPanelLayout(tabcount);
 
-				index.addTabPanels ( [ tabPanel ] );
-			}
+        index.addTabPanels([tabPanel]);
+      }
     });
   };
 }(jQuery));
