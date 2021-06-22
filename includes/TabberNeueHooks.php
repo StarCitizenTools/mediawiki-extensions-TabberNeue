a61702aec32b09cdc7d8dd7ca28409c6157b2142
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-06-22 13:16:44 -0600

    Clean up ParserFirstCallInit hokk

--- includes/TabberNeueHooks.php
+++ includes/TabberNeueHooks.php
@@ -18,13 +18,11 @@ class TabberNeueHooks {
 	/**
 	 * Sets up this extension's parser functions.
 	 *
-	 * @param Parser &$parser Parser object passed as a reference.
+	 * @param Parser $parser Parser object passed as a reference.
 	 *
-	 * @return bool true
 	 */
-	public static function onParserFirstCallInit( Parser &$parser ) {
-		$parser->setHook( 'tabber', 'TabberNeue\\TabberNeueHooks::renderTabber' );
-		return true;
+	public static function onParserFirstCallInit( Parser $parser ) {
+		$parser->setHook( 'tabber', [ __CLASS__, 'renderTabber' ] );
 	}
 
 	/**
