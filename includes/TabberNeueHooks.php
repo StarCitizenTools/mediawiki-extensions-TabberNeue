664c096bda6d23e06616c5d47388e231e246d6a3
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-06-22 13:17:46 -0600

    Fix

--- includes/TabberNeueHooks.php
+++ includes/TabberNeueHooks.php
@@ -19,7 +19,6 @@ class TabberNeueHooks {
 	 * Sets up this extension's parser functions.
 	 *
 	 * @param Parser $parser Parser object passed as a reference.
-	 *
 	 */
 	public static function onParserFirstCallInit( Parser $parser ) {
 		$parser->setHook( 'tabber', [ __CLASS__, 'renderTabber' ] );
