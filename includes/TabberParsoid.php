4e0e2bc767c95c0e7055f7dbb533fe75ed484900
alistair3149 <alistair3149@users.noreply.github.com>
2025-01-04 20:54:30 -0500

    refactor: remove unused code from TabberParsoid class
    TabberParsoid is currently unused but it is kept for development purpose.

--- includes/TabberParsoid.php
+++ includes/TabberParsoid.php
@@ -47,53 +47,6 @@ class TabberParsoid extends ExtensionTagHandler implements ExtensionModule {
 	 * @return string HTML
 	 */
 	public static function render( ParsoidExtensionAPI $extApi, string $src ): string {
-		$arr = explode( '|-|', $src );
-		$htmlTabs = '';
-		foreach ( $arr as $tab ) {
-			$htmlTabs .= self::buildTab( $extApi, $tab );
-		}
-
-		return '<div class="tabber">' .
-			'<header class="tabber__header"></header>' .
-			'<section class="tabber__section">' . $htmlTabs . "</section></div>";
-	}
-
-	/**
-	 * Build individual tab.
-	 *
-	 * @param ParsoidExtensionAPI $extApi
-	 * @param string $tab Tab information
-	 *
-	 * @return string HTML
-	 */
-	private static function buildTab( ParsoidExtensionAPI $extApi, string $tab ): string {
-		if ( empty( trim( $tab ) ) ) {
-			return '';
-		}
-
-		// Use array_pad to make sure at least 2 array values are always returned
-		[ $tabName, $tabBody ] = array_pad( explode( '=', $tab, 2 ), 2, '' );
-
-		/*
-		 * Use language converter to get variant title and also escape html
-		 * FIXME: No replacement method yet
-		 * See T85581, T272943
-		*/
-		// $tabName = $parser->getTargetLanguageConverter()->convertHtml( trim( $tabName ) );
-		$tabBody = $extApi->domToHTML(
-			$extApi->wikitextToDOM(
-				$tabBody,
-				[
-					'parseOpts' => [
-						'extTag' => 'tabber',
-						'context' => 'inline',
-					]
-				],
-				true // sol
-			)
-		);
-
-		return '<article class="tabber__panel" title="' . $tabName .
-			'">' . $tabBody . '</article>';
+		// TODO: Check Parsoid support
 	}
 }
