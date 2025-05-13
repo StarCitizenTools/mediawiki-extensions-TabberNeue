37495f7b62be11a034c96fcb3395c24281d6c396
alistair3149 <alistair31494322@gmail.com>
2025-05-12 20:48:16 -0400

    feat: use revision ID for fetching transclusion content
    Using the revision ID allows easier invalidation for CDN caching.

--- modules/ext.tabberNeue/ext.tabberNeue.js
+++ modules/ext.tabberNeue/ext.tabberNeue.js
@@ -91,8 +91,7 @@ class TabberAction {
 
 	/**
 	 * Sets the active tab panel in the tabber element.
-	 * Loads the content of the active tab panel if it contains a
-	 * `.tabber__transclusion` element with a `data-mw-tabber-load-url` attribute.
+	 * Loads the content of the active tab panel if it is a valid transclusion.
 	 * Adjusts the height of the section containing the active tab panel based on its content height.
 	 * Scrolls the section to make the active tab panel visible.
 	 *
